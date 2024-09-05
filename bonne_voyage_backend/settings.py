3f3dade1ca305d6b8a3025a6031cd3f7574874a8
Brien123 <zbrine1335@gmail.com>
2024-09-05 20:33:43 +0100

    ..

--- bonne_voyage_backend/settings.py
+++ bonne_voyage_backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['bonne_voyage_backend.onrender.com']
+ALLOWED_HOSTS = ['bonne-voyage.onrender.com']
 
 
 # Application definition
